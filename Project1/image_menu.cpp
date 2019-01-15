7c66c43834278145d93be0907f44f870f4e519d1
Samuel Carlson <tritium99@Hotmail.com>
2019-01-14 21:13:49 -0700

    Update image_menu.cpp

--- Project1/image_menu.cpp
+++ Project1/image_menu.cpp
@@ -2,36 +2,33 @@
 #include <iostream>
 #include "image_menu.h"
 
-std::string getString( std::istream& is, std::ostream& os, const std::string& prompt )
+std::string getString(std::istream& is, std::ostream& os, const std::string& prompt)
 {
 	os << prompt;
 	std::string line;
-	//std::getline(is, line);
 	is >> line;
 	return line;
 }
-int getInteger( std::istream& is, std::ostream& os, const std::string& prompt )
+int getInteger(std::istream& is, std::ostream& os, const std::string& prompt)
 {
 	os << prompt;
-	std::string line;
-	//std::getline(is, line);
+	int line;
 	is >> line;
-	return std::stoi(line);
+	return line;
 }
-double getDouble( std::istream& is, std::ostream& os, const std::string& prompt )
+double getDouble(std::istream& is, std::ostream& os, const std::string& prompt)
 {
 	os << prompt;
-	std::string line;
-	//getline(is, line);
+	double line;
 	is >> line;
-	return std::stod(line);
+	return line;
 }
-int assignment1( std::istream& is, std::ostream& os )
+int assignment1(std::istream& is, std::ostream& os)
 {
 	std::string s = getString(is, os, "What's your favorite color? ");
 	int integer = getInteger(is, os, "What's your favorite integer? ");
-	double d = getDouble(is, os, "What's your favorite double? ");
+	double d = getDouble(is, os, "What's your favorite number? ");
 	for (int i = 1; i <= integer; i++)
 		os << i << ' ' << s << ' ' << d << std::endl;
-	return 1;
+	return integer;
 }
