74f5d731c80db46e91474761fe8a5eb4e2df6712
Samuel Carlson <tritium99@Hotmail.com>
2019-01-23 10:04:24 -0700

    Added writeUserImage function declaration

--- Project2/image_menu.h
+++ Project2/image_menu.h
@@ -8,6 +8,7 @@ std::string getString(std::istream& is, std::ostream& os, const std::string& pro
 int getInteger(std::istream& is, std::ostream& os, const std::string& prompt);
 double getDouble(std::istream& is, std::ostream& os, const std::string& prompt);
 int assignment1(std::istream& is, std::ostream& os);
+void writeUserImage(std::istream& is, std::ostream& os, const PPM& p);
 int assignment2(std::istream& is, std::ostream& os);
 
 #endif // _IMAGE_MENU_H_
