aaa4d68aff3ecb6d513911f5a324c98ec4ace613
Samuel Carlson <samwise.carlson@gmail.com>
2019-02-14 12:30:29 -0700

    Added optional, faster, less careful code

--- Project5/PPM.cpp
+++ Project5/PPM.cpp
@@ -214,6 +214,9 @@ PPM PPM::operator+(const PPM& ppm_object) const
 				ppm3.setChannel(i, j, k, new_value);
 			}
 	return ppm3;
+	/*PPM ppm = *this;
+	ppm += ppm_object;
+	return ppm;*/
 }
 // The operator to subtract the values of two PPM objects, creating a new one. e.g. ppm3 = ppm1 - ppm2; Assume the PPM objects are of the same size.
 PPM PPM::operator-(const PPM& ppm_object) const
@@ -230,14 +233,24 @@ PPM PPM::operator-(const PPM& ppm_object) const
 				ppm3.setChannel(i, j, k, new_value);
 			}
 	return ppm3;
+	/*PPM ppm = *this;
+	ppm -= ppm_object;
+	return ppm;*/
 }
 /* The operator to add one PPM object into another. e.g. ppm1 += ppm2. This should add each pixel of the images.
    A pixel addition is completed by adding the red channels, adding the green channels and adding the blue channels.
    If any channel exceeds the maximum color value, set the channel to the maximum color value. Assume the PPM objects are of the same size.*/
 PPM& PPM::operator+=(const PPM& ppm_object)
 {
-	*this = (*this + ppm_object);
+	 *this = (*this + ppm_object);
 	return *this;
+	// If the sizes are guarenteed to be equal, I could use this since I wouldn't need the safety checks of getChannel
+	/*for (size_t i = 0; i < sizeOfImage(); i++)
+	{
+		int new_value = image[i] + ppm_object.image[i];
+		image[i] = (new_value > max_color_value ? max_color_value : new_value);
+	}
+	return *this;*/
 }
 /* The operator to subtract one PPM object from another. e.g. ppm1 -= ppm2. This should subtract each pixel of the images.
    A pixel subtraction is completed by subtracting the red channels, subtracting the green channels and subtracting the blue channels.
@@ -246,6 +259,12 @@ PPM& PPM::operator-=(const PPM& ppm_object)
 {
 	*this = (*this - ppm_object);
 	return *this;
+	/*for (size_t i = 0; i < sizeOfImage(); i++)
+	{
+		int new_value = image[i] - ppm_object.image[i];
+		image[i] = (new_value < 0 ? 0 : new_value);
+	}
+	return ppm*/
 }
 // The operator to multiply the values of a PPM object by a double, creating a new PPM object. e.g. ppm3 = ppm1 * 0.67;
 PPM PPM::operator*(const double& value) const
@@ -264,11 +283,22 @@ PPM PPM::operator*(const double& value) const
 				ppm3.setChannel(i, j, k, new_value);
 			}
 	return ppm3;
+	/*PPM ppm = *this;
+	ppm *= value;
+	return ppm;*/
 }
 PPM& PPM::operator*=(const double& value)
 {
 	*this = (*this * value);
 	return *this;
+	/*for (size_t i = 0; i < sizeOfImage(); i++)
+	{
+		int new_value = image[i] * value;
+		if (new_value > max_color_value) new_value = max_color_value;
+		else if (new_value < 0) new_value = 0;
+		image[i] = new_value;
+	}
+	return *this;*/
 }
 // The operator to divide the values of a PPM object by a double, creating a new PPM object. e.g. ppm3 = ppm1 / 0.25;
 PPM PPM::operator/(const double& value) const
@@ -280,8 +310,7 @@ PPM PPM::operator/(const double& value) const
    If any channel would exceed the maximum color value, set it to the maximum color value. If any channel would become less than 0, set it to 0.*/
 PPM& PPM::operator/=(const double& value)
 {
-	*this = (*this / value);
-	return *this;
+	return *this *= (1 / value);
 }
 bool PPM::operator== (const PPM& ppm_object) const
 {
